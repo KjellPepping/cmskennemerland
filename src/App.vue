fa7484ffcfccd00f06bb0beea651f0e1dfeb6ad9
KjellPepping <kjellpepp2000@gmail.com>
2021-10-19 15:40:57 +0200

    initialize

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app>
+    <v-app-bar color="deep-purple" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Kennemerland CMS {{ $route.name }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+    >
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item href="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+              <v-list-item href="/user">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>User</v-list-item-title>
+          </v-list-item>
+
+              <v-list-item href="/task">
+            <v-list-item-icon>
+              <v-icon>mdi-calendar-check</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Task</v-list-item-title>
+          </v-list-item>
+
+              <v-list-item href="/goal">
+            <v-list-item-icon>
+              <v-icon>mdi-flag-checkered</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Goal</v-list-item-title>
+          </v-list-item>
+
+              <v-list-item href="/report">
+            <v-list-item-icon>
+              <v-icon>mdi-chart-areaspline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Report</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+       <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  data: () => ({
+      drawer: false,
+      group: null,
+    }),
+  name: 'App',
+
+  components: {
+
+    
+  },
+
+
+};
+</script>
